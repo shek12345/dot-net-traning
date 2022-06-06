b9ed2d4f16b77a9fed2b7e99e56e2450e3cb13f9
shek12345 <shekharshinde343@gmail.com>
2022-06-06 10:31:22 +0530

    1

---
+++ dot net traning/dot net 4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dot_net_traning
+{
+    internal class dot_net_4
+    {
+    }
+}
